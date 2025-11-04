68848f06e123a12d9e4cdf0586c21a8ae01163b8
ABHISHEK-SINGH-KASHYAP <abhisheksinghkashyap91@gmail.com>
2025-11-04 14:25:59 +0530

    DB sql lite first try

--- apps/employees_sql.py
+++ apps/employees_sql.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, Float, create_engine
 
 Base = declarative_base()
 
@@ -9,10 +9,21 @@ class Employee(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     job_title = Column(String(150), nullable=False)
-    salary = Column(Float)
+    salary = Column(Float, nullable=False)
     
     def __repr__(self):
         return f'[id = {self.id}, name = {self.name}, job_title = {self.job_title}, salary = {self.salary}]'
     
-    def __str__(self):
-        return self.__repr__()
+#setup database
+engine = create_engine('sqlite:///employees_db.sqlite',echo=True) #create database if not exist
+Base.metadata.create_all(engine)
+
+#setup the things for the transactions(create, read, update, delete)
+Sessionlocal = sessionmaker(bind=engine)
+session = Sessionlocal()
+
+#crudops
+dravid = Employee(name = 'Dravid', job_title = 'Batsman', salary = 1200)
+session.add(dravid)
+session.commit()
+
